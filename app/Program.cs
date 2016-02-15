4d50ea10d400bdd833d56254932da0850431c9ef
unknown <ROMAINRA@ROMAINRA02.corp.amdocs.com>
2016-02-15 17:40:18 +0100

    Handled contextual submenu & package extract

--- app/Program.cs
+++ app/Program.cs
@@ -2,65 +2,80 @@
 using System.IO;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace PortableR
 {
-    class Program
-    {
-        const string logFile = "debug.log";
-        static StreamWriter logInstance;
-        static bool hadError = false;
+	class Program
+	{
+		const string logFile = "debug.log";
+		static string extractFolder = "%UserProfile%\\Apps\\";
+		static StreamWriter logInstance;
+		static bool hadError = false;
+		static string portablerExe, portablerFolder;
+		static string appExe, appName, appDir;
+		static string cmd, parameters;
 
-        static void Main(string[] args)
-        {
-            // init
-            string[] arguments = Environment.GetCommandLineArgs();
-            string portablerExe = arguments[0]; // C:\Projects\portabler\app\bin\PortableR.exe
-            string portablerFolder = Path.GetDirectoryName(portablerExe) + "\\"; // C:\Projects\portabler\app\bin\
-            string action = arguments.Length > 1 ? arguments[1] : "";
-            logInstance = new StreamWriter(portablerFolder + logFile);
-            logInstance.AutoFlush = true;
+		static void Main()
+		{
+			// init
+			string[] arguments = Environment.GetCommandLineArgs();
+			portablerExe = Application.ExecutablePath; // C:\Projects\portabler\app\bin\PortableR.exe
+			portablerFolder = Path.GetDirectoryName(Application.ExecutablePath) + "\\";  // C:\Projects\portabler\app\bin\			
+			extractFolder = Environment.ExpandEnvironmentVariables(extractFolder);	
+			string action = arguments.Length > 1 ? arguments[1] : "";
+			logInstance = new StreamWriter(portablerFolder + logFile);
+			logInstance.AutoFlush = true;
+			if (arguments.Length > 2) {
+				appExe = arguments[2];
+				appName = Path.GetFileName(appExe);
+				appDir = Path.GetDirectoryName(appExe) + "\\";
+			}
 
-            // splash
-            Log("\n", "draw");
-            Log("   ___           _        _     _        __  ", "draw");
-            Log("  / _ \\___  _ __| |_ __ _| |__ | | ___  /__\\ ", "draw");
-            Log(" / /_)/ _ \\| '__| __/ _` | '_ \\| |/ _ \\/ \\// ", "draw");
-            Log("/ ___/ (_) | |  | || (_| | |_) | |  __/ _  \\ ", "draw");
-            Log("\\/    \\___/|_|   \\__\\__,_|_.__/|_|\\___\\/ \\_/ ", "draw");
+			// splash
+			Log("\n", "draw");
+			Log("   ___           _        _     _        __  ", "draw");
+			Log("  / _ \\___  _ __| |_ __ _| |__ | | ___  /__\\ ", "draw");
+			Log(" / /_)/ _ \\| '__| __/ _` | '_ \\| |/ _ \\/ \\// ", "draw");
+			Log("/ ___/ (_) | |  | || (_| | |_) | |  __/ _  \\ ", "draw");
+			Log("\\/    \\___/|_|   \\__\\__,_|_.__/|_|\\___\\/ \\_/ ", "draw");
 
-            // log parameters
-            Log("Variables", "title");
-            Log("arguments : " + String.Join(", ", arguments));
-            Log("portablerExe : " + portablerExe);
-            Log("portablerFolder : " + portablerFolder);
+			// log parameters
+			Log("Variables", "title");
+			Log("arguments : " + String.Join(", ", arguments));
+			Log("portablerExe : " + portablerExe); // C:\Projects\portabler\app\bin\PortableR.exe
+			Log("portablerFolder : " + portablerFolder); // C:\Projects\portabler\app\bin\
+			Log("extractFolder : " + extractFolder);
+			Log("appExe : " + appExe);
+			Log("appName : " + appName);
+			Log("appDir : " + appDir);
 
-            if (action.Length > 0)
-            {
-                Log("action : " + action);
-            }
+			if (action.Length > 0) {
+				Log("action : " + action);
+			}
 
 			switch (action) {
+            		
 				case "install":
-					Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.ClassesRoot;
+					var key = Microsoft.Win32.Registry.ClassesRoot;
 					try {
-						key = key.CreateSubKey("exefile\\shell\\PortableR\\command");
-						key.SetValue("", arguments[0] + " create \"%0\"");
+						key = key.CreateSubKey("exefile\\shell\\PortableR");
+						key.SetValue("MUIVerb", "PortableR");
+						key.SetValue("SubCommands", "PortableRcreate;PortableRextract");
+						key.SetValue("icon", portablerExe);	
 					} catch (Exception ex) {
-						Log("error during wrinting to reg : " + ex, "error");
+						Log("error during wrinting to root reg : " + ex, "error");
 					} finally {
 						key.Close();
 					}
+					CreateSubCommand("Create package", "create");
+					CreateSubCommand("Extract package", "extract");
 					break;
-				case "create":
-					string appExe = arguments[2];
-					string appName = Path.GetFileName(appExe);
-					string appDir = Path.GetDirectoryName(appExe);
-					Log("appExe : " + appExe);
-					Log("appName : " + appName);
-					Log("appDir : " + appDir);
+					
+				case "create":								
 					const string portableAppTemp = "portableApp.exe";
-					string portableAppName = appDir.Substring(appDir.LastIndexOf('\\') + 1).Replace(" ", ".");
+					string dir = Path.GetDirectoryName(appExe);
+					string portableAppName = dir.Substring(dir.LastIndexOf('\\') + 1).Replace(" ", ".");
 					var versionInfo = FileVersionInfo.GetVersionInfo(appExe);
 					string version = string.IsNullOrEmpty(versionInfo.ProductVersion) ? "0" : versionInfo.ProductVersion;
 					version = Regex.Replace(version, @"\D", "");
@@ -68,7 +83,6 @@ namespace PortableR
 					fileVersion = Regex.Replace(fileVersion, @"\D", "");
 					string finalVersion = (int.Parse(version) > int.Parse(fileVersion)) ? version : fileVersion;
 					portableAppName += ("_" + (finalVersion + "000").Substring(0, 3) + ".exe");
-					appDir += "\\";
 					Log("version : " + version);
 					Log("fileVersion : " + fileVersion);
 					Log("finalVersion : " + finalVersion);
@@ -81,8 +95,7 @@ namespace PortableR
 					configSFX.WriteLine("Title=Portable App by PortableR");
 					configSFX.WriteLine("Setup=" + appName);
 					configSFX.Flush();
-					configSFX.Close();
-					string cmd, parameters;
+					configSFX.Close();					
 					try {
 						cmd = portablerFolder + "third-party\\" + "rar.exe";
 						parameters = "a -ep -ep1 -r -sfx\"" + portablerFolder + "Default.sfx\" -z\"" + portablerFolder + "ConfigSFX.txt\" \"" + appDir + portableAppTemp + "\" \"" + appDir + "*\"";
@@ -143,31 +156,65 @@ namespace PortableR
 						Log("error while cleaning temp files : " + ex.Message, "error");
 					}
 					break;
+					
+				case "extract":
+					try {
+						string folderName = appName.Substring(0, appName.LastIndexOf("_", StringComparison.Ordinal)) + "\\";												
+						cmd = portablerFolder + "third-party\\" + "rar.exe";
+						parameters = "x  " + "\"" + appExe + "\"" + " " + "\"" + extractFolder + folderName + "\"";
+						Log("App Extract", "title");
+						Log("starting cmd : " + cmd);
+						Log("with params : " + parameters);
+						Process extractProcess = Process.Start(cmd, parameters);
+						extractProcess.WaitForExit();
+					} catch (Exception ex) {
+						Log("error while extracting app : " + ex.Message, "error");
+					}
+					break;
+					
 				case "":
 					Log("no action given");
 					break;
+					
 				default:
 					Log("non handled action");
 					break;
+					
 			}
 
-            // close log
-            Log("PortableR end", "end");
+			// close log
+			Log("PortableR end", "end");
 
-            if (hadError)
-            {
-                System.Windows.Forms.MessageBox.Show("Some error(s) happended, look at " + portablerFolder + logFile);
-            }
-        }
+			if (hadError) {
+				System.Windows.Forms.MessageBox.Show("Some error(s) happended, look at " + portablerFolder + logFile);
+			}
+		}
 
-        static string logPrefix, logSuffix, logSpaces;
-        const int logMargin = 18;
+		static void CreateSubCommand(string title, string command)
+		{
+			var key = Microsoft.Win32.Registry.LocalMachine;
+			try {
+				key = key.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CommandStore\\shell\\PortableR" + command);
+				key.SetValue("", title);
+				key.SetValue("icon", portablerFolder + command + ".ico");	
+				key = key.CreateSubKey("command");
+				key.SetValue("", portablerExe + " " + command + " \"%0\"");
+			} catch (Exception ex) {
+				Log("error during wrinting \"" + command + "\" command to local machine reg : " + ex, "error");
+			} finally {
+				key.Close();
+			}
+		}
+		
+		
+		static string logPrefix, logSuffix, logSpaces;
+		const int logMargin = 18;
 
-        static void Log(string str, string type = "debug")
-        {
+		static void Log(string str, string type = "debug")
+		{
 
-            logPrefix = "";
-            logSuffix = "";
+			logPrefix = "";
+			logSuffix = "";
 
 			switch (type) {
 				case "error":
@@ -190,23 +237,21 @@ namespace PortableR
 					break;
 			}
 
-            int index = str.IndexOf(":");
-            if (index > 0)
-            {
-                logSpaces = new string(' ', logMargin - index);
-                str = str.Substring(0, index) + logSpaces + " : " + str.Substring(index + 1);
-            }
+			int index = str.IndexOf(":", StringComparison.Ordinal);
+			if (index > 0) {
+				logSpaces = new string(' ', logMargin - index);
+				str = str.Substring(0, index) + logSpaces + " : " + str.Substring(index + 1);
+			}
 
-            logInstance.WriteLine(logPrefix + str + logSuffix);
+			logInstance.WriteLine(logPrefix + str + logSuffix);
 
-            logInstance.Flush();
+			logInstance.Flush();
 
-            if (type == "end")
-            {
-                logInstance.Close();
-            }
-        }
+			if (type == "end") {
+				logInstance.Close();
+			}
+		}
 
-    }
+	}
 
 }
